3439072667c4cfa9d47dbc75549cbd000deb9039
Peter Heslin <pheslin@gmail.com>
2020-12-22 22:46:15 +0000

    Add treemacs.

--- easymacs.el
+++ easymacs.el
@@ -344,6 +344,37 @@ the mode doesn't support imenu."
           (lambda ()
             (define-key org-mode-map (kbd "<C-tab>") nil)))
 
+;; Treemacs
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-is-never-other-window nil)
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t))
+  :bind* (("<C-f8>"   . treemacs)
+          ("<C-S-f8>" . easymacs-dwim-treemacs)))
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+(defun easymacs-dwim-treemacs ()
+  (interactive)
+  (if (treemacs-is-treemacs-window? (selected-window))
+      (other-window 1)
+    (treemacs-select-window)))
+
 ;;; Utility functions
 
 (defun unfill-paragraph (&optional region)
